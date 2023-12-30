346b1560ae50a21f4a81d00d2dbc44f2d57b5ee5
yogeshNavghane67 <yogeshnavghane661@gmail.com>
2023-12-30 18:00:16 +0530

    QR code box

--- client/src/account/LoginDialog.jsx
+++ client/src/account/LoginDialog.jsx
@@ -1,4 +1,38 @@
-import { Dialog } from "@mui/material";
+import { Dialog, Box, Typography, List, ListItem, styled } from "@mui/material";
+
+import { qrCodeImage } from "../constants/data";
+
+const Component =styled(Box)`
+display: flex;
+`;
+const Container = styled(Box)`
+padding: 56px 0 56px 56px;
+`;
+
+const QRCode = styled('img')({
+    height: 264,
+    width: 264,
+    margin: '50px 0 0 50px',
+})
+
+const Title = styled(Typography)`
+font-size: 26px;
+color: #525252;
+font-weight: 300;
+font-family: inherit;
+margin-bottom: 25px;
+`;
+
+const StyledList = styled(List)`
+& > li {
+    padding: 0;
+    margin-top: 15px;
+    font-size: 18px;
+    line-height: 28px;
+    color: #4a4a4a
+}
+`
+
 
 const dialogStyle = {
     height: '96%',
@@ -7,14 +41,28 @@ const dialogStyle = {
     maxWidth: '100%',
     maxHeight: '100%',
     boxShadow: 'none',
-    overflow: 'none'
-
+    overflow: 'hidden'
 }
 
 
 const LoginDialog = () => {
     return (
-        <Dialog open={true} PaperProps={{ sx: dialogStyle }}>Hello</Dialog>
+        <Dialog open={true} PaperProps={{ sx: dialogStyle }}>
+        <Component>
+            <Container>
+                <Title>Use WhatsApp on your computer</Title>
+                <StyledList>
+                    <ListItem>1. Open WhatsApp on your phone</ListItem>
+                    <ListItem>2. Tap Menu settings and select WhatsApp web</ListItem>
+                    <ListItem>3. Posint your phone to this screen to caption the code</ListItem>
+
+                </StyledList>
+            </Container>
+            <Box>
+                <QRCode src={qrCodeImage} alt="qr code"/>
+            </Box>
+        </Component>
+        </Dialog>
     )
 }
 
